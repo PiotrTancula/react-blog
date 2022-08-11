e650772fdcb5d30a32a6428f3fe6e2163f1e0e2c
Piotr Tańcula <tanculapiotr@gmail.com>
2022-08-11 12:52:54 +0200

    delete post fixes

--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -5,17 +5,14 @@ export const getPostById = ({ posts }, postId) => posts.find((post) => post.id =
 const createActionName = actionName => `app/posts/${actionName}`;
 const DELETE_POST = createActionName('DELETE_POST');
 
-export const deletePost = (payload) => ({
-  type: DELETE_POST,
-  payload,
-});
+export const deletePost = (payload) => ({type: DELETE_POST,payload});
 
 
 // action creators
 const postsReducer = (statePart = [], action) => {
   switch (action.type) {
     case DELETE_POST:
-    return [...statePart].filter((post) => post.id !== action.payload.postId);
+    return [...statePart.filter(post => post.id !== action.payload)];
     default:
       return statePart;
   };
